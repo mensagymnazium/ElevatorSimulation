e476c5d8e4be08ba826fa1cbc34f8009e254fe2b
ukraine584 <48248684+mrxbox1@users.noreply.github.com>
2025-11-19 17:43:13 +0100

    asdf

---
+++ ElevatorSimulation/Strategies/GABRIEL_DRUNKELEVATOR.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+
+namespace ElevatorSimulation.Strategies;
+
+public class MojeStrategieStrategy : IElevatorStrategy
+{
+    public MoveResult DecideNextMove(ElevatorSystem elevator)
+    {
+        //vodka.cs
+
+        Random rnd = new Random();
+        int elevator_mood = rnd.Next(-1, 1);
+
+        if (elevator_mood == -1)
+        {
+            return MoveResult.MoveDown;
+        }
+        else if (elevator_mood == 1)
+        {
+            return MoveResult.MoveUp;
+        }
+        else
+        {
+            return MoveResult.OpenDoors;
+        }
+    }
+}
